7bbfa2e6f5ce212295a9a8e6453a9f2f313b7571
Afnan Enayet <afnan.enayet@gmail.com>
2015-09-19 23:17:03 -0400

    -Removed service as it is unnecessary for a simple action -Added a new setting in preferences to toggle RTC and RTC_WAKELOCK -Added corresponding strings for new preferences -Fixed alarm receiver to receive vibration duration from intent broadcasted by the original activity -Added a menu item to settings activity to stop the vibration alarm service -Updated android manifest to correctly request the vibration permission -Added more comments to improve readability -Added log files for better debugging aid from the ADB logcat

--- app/src/main/java/com/enayet/bendroid/AlarmReceiver.java
+++ app/src/main/java/com/enayet/bendroid/AlarmReceiver.java
@@ -3,7 +3,9 @@ package com.enayet.bendroid;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Vibrator;
+import android.util.Log;
 
 public class AlarmReceiver extends BroadcastReceiver {
     public AlarmReceiver() {
@@ -11,8 +13,11 @@ public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        //intent.getIntExtra("vibrationDuration", 1); //TODO get int from intent, use that as vibration length
+        Log.i("BenDroid/AlarmReceiver", "Alarm received successfully"); //TODO: remove
+        //Toast.makeText(AlarmReceiver.this, R.string.vibration_toast, Toast.LENGTH_SHORT).show(); //TODO: get context of toast
+        Bundle extras = intent.getExtras();
+        int mDuration = extras.getInt("vibrationDuration"); //Gets duration int from broadcaster
         Vibrator mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-        mVibrator.vibrate(500); //vibrate for milliseconds specified in argument TODO change duration to variable
+        mVibrator.vibrate(mDuration); //vibrate for milliseconds specified in argument
     }
 }
